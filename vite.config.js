56646836db439c1687009e020209b338f785646e
Ch1n3du <danielonyesoh@gmail.com>
2023-12-10 22:33:25 +0100

    chore(hosting): Fixed url base issue.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/AfriHacks2023-rollbucks_frontend/"
+  base: "https://rollbucks.github.io/AfriHacks2023-rollbucks_frontend/"
 })
