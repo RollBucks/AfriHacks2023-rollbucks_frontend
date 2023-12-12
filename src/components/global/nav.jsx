adf10a19946a8346b0f780bc4cc411176b7cddde
tanthehack <31630356+tanthehack@users.noreply.github.com>
2023-12-12 23:54:08 +0100

    add: dashboard link in nav

--- src/components/global/nav.jsx
+++ src/components/global/nav.jsx
@@ -18,6 +18,7 @@ export const Nav = () => {
             <li className='hover:underline'><a href='/#home' className='hover:text-blue-300'>Home</a></li>
             <li className='hover:underline'><a href='/#about' className='hover:text-blue-300'>About</a></li>
             <li className='hover:underline'><a href='/#features' className='hover:text-blue-300'>Features</a></li>
+            <li className='hover:underline'><Link to='/dashboard' className='hover:text-blue-300'>Dashboard</Link></li>
             {/* <li><a href='/#faq' className='hover:text-blue-300'>FAQ</a></li>
             <li><a href='/#contact' className='hover:text-blue-300'>Contact</a></li> */}
         </ul>
